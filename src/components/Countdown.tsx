ec3ffebe19156e51b05f7c254b0fe95b3aeda468
SavioHub <saviosantossilva19@gmail.com>
2021-04-02 17:10:58 -0300

    update sexta3

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -11,7 +11,7 @@ export function Countdown(){
         hasFinished, 
         startCountdown, 
         resetCountdown, 
-        isActive
+        isActive,
     } = useContext(CountdownContext)
 
     const [minuteLeft, minuteRight] =String(minutes).padStart(2,'0').split('');// pad start add um 0 a esquerda quando minute tem apenas uma casa (padão duas casas)
